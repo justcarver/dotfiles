3dd6d3fe7e6cc6adafa3330f76aa73ff2cf3c3e1
Justin Carver <carver@justcarver.com>
2019-05-03 16:19:48 -0400

    Changed tm function to use fzf

--- .config/fish/functions/tm.fish
+++ .config/fish/functions/tm.fish
@@ -2,6 +2,7 @@ function tm -a 'sessionname'
   if test -n "$sessionname"
     tmux attach -t $sessionname || tmux new -s $sessionname
   else
-    tmux attach -t dev || tmux new -s dev
+    set -lx SESSION (tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0)
+    tmux attach -t $SESSION || echo "No sessions found"
   end
 end
